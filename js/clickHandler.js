60c0570329117019fd479bf85b5c84bfa7ad1d23
John Currie <johntcurrie@gmail.com>
2017-07-25 09:25:22 -0700

    Added jquery click handler to toggle arrow images

---
+++ js/clickHandler.js
@@ -0,0 +1,10 @@
+var open = false;
+$(".arrow-button").click(function() {
+	if(open) {
+		$(".arrow-button").attr('src', 'Assets/Collapse_Arrow.png');
+		open = false;
+	} else {
+		$(".arrow-button").attr('src', 'Assets/Expand_Arrow.png');
+		open = true;
+	}
+})
